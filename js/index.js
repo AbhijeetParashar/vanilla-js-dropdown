cf9f38ff1f95336eeea59ef3b0058f692d41ed06
Abhijeet Kumar <53019674+AbhijeetParashar@users.noreply.github.com>
2020-07-07 15:54:03 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Import CustomSelect
+import CustomSelect from 'vanilla-js-dropdown';
+
+// Import stylesheets
+import '../css/main.css';
+
+// Boom!
+var simpleSelect = new CustomSelect({
+  elem: 'select'
+});
+
+var optgroupSelect = new CustomSelect({
+  elem: 'select-optgroup'
+});
